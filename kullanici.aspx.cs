8eb9ddacd865210652754860092631da6a40ce54
berkaycelik66 <berkaycelik66@hotmail.com>
2023-12-18 22:28:18 +0300

    kullanici.aspx added

---
+++ kullanici.aspx.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace YarismaSitesi
+{
+    public partial class kullanici : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+            object user = Session["username"];
+            String username = Request.QueryString["uname"];
+
+            /*if (username == user.ToString())
+            {
+
+            }
+            else
+            {
+                Response.Redirect("anasayfa.aspx");
+            }*/
+        }
+    }
+}
