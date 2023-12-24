b7ba83fb6c0640b4547f290cae05d43b5eeddfb6
berkaycelik66 <berkaycelik66@hotmail.com>
2023-12-24 13:26:27 +0300

    admin panel added

---
+++ admin.aspx
@@ -0,0 +1,87 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/anasayfa1.Master" AutoEventWireup="true" CodeBehind="admin.aspx.cs" Inherits="YarismaSitesi.admin" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+    <style>
+        .paginationPanel {
+            display: flex;
+            list-style: none;
+            padding: 0;
+            justify-content: center;
+            margin-top: 20px;
+        }
+
+            .paginationPanel a {
+                color: black;
+                float: left;
+                padding: 8px 16px;
+                text-decoration: none;
+                transition: background-color .3s;
+                border: 1px solid #ddd;
+                border-radius: 5px;
+                margin: 0 4px;
+            }
+
+                .paginationPanel a.active {
+                    background-color: #4CAF50;
+                }
+
+                .paginationPanel a:hover:not(.active) {
+                    background-color: #ddd;
+                }
+    </style>
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+
+
+    <table class="admin" border="1">
+
+        <tr>
+            <td>Kategori</td>
+            <td>Sorular</td>
+            <td>Doğru Cevap</td>
+            <td>Onay</td>
+        </tr>
+
+        <asp:Repeater ID="rpt1" runat="server">
+            <ItemTemplate>
+
+                <tr>
+                    <td><%#Eval("category")%></td>
+                    <td><%#Eval("question")%></td>
+                    <td><%#Eval("true_answer")%></td>
+                    <td><%#Eval("state").Equals(true) ? "Onaylandı": "Onaylanmadı"%></td>
+                    <td><a href="soru-duzenle.aspx?id=<%#Eval("id")%>">Düzenle</a></td>
+                    <td><a href="soru-sil.aspx?id=<%#Eval("id")%>">Sil</a></td>
+                    <td><a href="soru-onay.aspx?id=<%#Eval("id")%>">Onayla</a></td>
+                </tr>
+            </ItemTemplate>
+        </asp:Repeater>
+    </table>
+    <asp:Panel ID="pnlsyf" runat="server" CssClass="paginationPanel">
+    </asp:Panel>
+
+
+    <table class="admin" border="1">
+
+        <tr>
+            <td>Email</td>
+            <td>Kullanıcı Adı</td>
+        </tr>
+
+        <asp:Repeater ID="rpt2" runat="server">
+            <ItemTemplate>
+                <tr>
+                    <td><%#Eval("mail")%></td>
+                    <td><%#Eval("username")%></td>
+                    <td>
+                        <%# Eval("task").Equals("banned") ? "<a href='engel.aspx?id=" + Eval("id") + "'>Engeli Kaldır</a>" : "<a href='engel.aspx?id=" + Eval("id") + "'>Engelle</a>" %>
+                    </td>
+
+                </tr>
+            </ItemTemplate>
+        </asp:Repeater>
+    </table>
+    <asp:Panel ID="pnlsyf2" runat="server" CssClass="paginationPanel">
+    </asp:Panel>
+
+</asp:Content>
---
+++ admin.aspx.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace YarismaSitesi
+{
+    public partial class admin : System.Web.UI.Page
+    {
+        SqlConnection baglan = new SqlConnection("Data Source=DESKTOP-USOAJ0L\\SQLEXPRESS;Initial Catalog=yarisma;Integrated Security=True");
+        //SqlConnection baglan = new SqlConnection("Data Source=DESKTOP-GP90RBV\\SQLEXPRESS;Initial Catalog=yarisma;Integrated Security=True");
+
+        int soruSayfa = 1;
+        int userSayfa = 1;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                soruPanel();
+                kullaniciPanel();
+            }
+        }
+
+        protected void soruPanel()
+        {
+            baglan.Open();
+            SqlDataAdapter da = new SqlDataAdapter("select * from questions", baglan);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+
+            PagedDataSource pds = new PagedDataSource();
+            pds.DataSource = dt.DefaultView;
+            pds.AllowPaging = true; //Sayfalama aktif olması için
+            pds.PageSize = 2; //Bir sayfadaki kayıt sayısı
+
+
+            if (Request.QueryString["soruSayfa"] != null)
+            {
+                soruSayfa = Convert.ToInt32(Request.QueryString["soruSayfa"]);
+            }
+            else
+            {
+                soruSayfa = 1;
+            }
+
+            pds.CurrentPageIndex = soruSayfa - 1;
+
+            for (int i = 1; i <= pds.PageCount; i++)
+            {
+                HyperLink hyper = new HyperLink();
+                hyper.Text = i.ToString();
+                hyper.NavigateUrl = "admin.aspx?soruSayfa=" + i + "&userSayfa=" + userSayfa;
+
+                pnlsyf.Controls.Add(hyper);
+            }
+
+            rpt1.DataSource = pds;
+            rpt1.DataBind();
+            baglan.Close();
+        }
+
+        protected void kullaniciPanel()
+        {
+            baglan.Open();
+            SqlDataAdapter da = new SqlDataAdapter("select * from users", baglan);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+
+            PagedDataSource pds = new PagedDataSource();
+            pds.DataSource = dt.DefaultView;
+            pds.AllowPaging = true; //Sayfalama aktif olması için
+            pds.PageSize = 2; //Bir sayfadaki kayıt sayısı
+
+
+            if (Request.QueryString["userSayfa"] != null)
+            {
+                userSayfa = Convert.ToInt32(Request.QueryString["userSayfa"]);
+            }
+            else
+            {
+                userSayfa = 1;
+            }
+
+            pds.CurrentPageIndex = userSayfa - 1;
+
+            for (int i = 1; i <= pds.PageCount; i++)
+            {
+                HyperLink hyper = new HyperLink();
+                hyper.Text = i.ToString();
+                hyper.NavigateUrl = "admin.aspx?soruSayfa=" + soruSayfa + "&userSayfa=" + i;
+
+                pnlsyf2.Controls.Add(hyper);
+            }
+
+            rpt2.DataSource = pds;
+            rpt2.DataBind();
+            baglan.Close();
+        }
+    }
+}
