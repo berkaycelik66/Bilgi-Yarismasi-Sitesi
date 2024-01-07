e836f98193434dea8b9e772c6b213e75e234c330
abdlylmz5861 <abdullah.ylmz5861@gmail.com>
2024-01-07 15:51:34 +0300

    yarisma.aspx script added

--- yarisma.aspx.cs
+++ yarisma.aspx.cs
@@ -12,8 +12,8 @@ namespace YarismaSitesi
 {
     public partial class yarisma : System.Web.UI.Page
     {
-        SqlConnection baglan = new SqlConnection("Data Source=DESKTOP-USOAJ0L\\SQLEXPRESS;Initial Catalog=yarisma;Integrated Security=True");
-        //SqlConnection baglan = new SqlConnection("Data Source=DESKTOP-GP90RBV\\SQLEXPRESS;Initial Catalog=yarisma;Integrated Security=True");
+        //SqlConnection baglan = new SqlConnection("Data Source=DESKTOP-USOAJ0L\\SQLEXPRESS;Initial Catalog=yarisma;Integrated Security=True");
+        SqlConnection baglan = new SqlConnection("Data Source=DESKTOP-GP90RBV\\SQLEXPRESS;Initial Catalog=yarisma;Integrated Security=True");
 
         static List<Question> questionBatch;
         static int currentQuestionIndex;
@@ -21,6 +21,11 @@ namespace YarismaSitesi
         {
             if (!IsPostBack)
             {
+                if (questionBatch != null)
+                {
+                    questionBatch.Clear();
+                    currentQuestionIndex = questionBatch.Count();
+                } 
                 Button5.Visible = false;
                 LoadQuestionBatch();
                 InitializeQuestion();
